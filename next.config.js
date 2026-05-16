2e3c7ada69984a49b51647a5320cc651f8843887
Duncan <eduworldkenya@gmail.com>
2026-05-16 21:33:05 +0300

    fix: remove babelrc to restore next/font compatibility

--- next.config.js
+++ next.config.js
@@ -1,9 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  experimental: {
-    forceSwcTransforms: false,
-  },
-  swcMinify: false,
-};
+const nextConfig = {};
 
 module.exports = nextConfig;
