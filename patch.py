23949d3eec3f1e79670c4bdebc5da327fac60a55
Duncan <eduworldkenya@gmail.com>
2026-05-28 13:14:11 +0300

    chore: remove junk files

--- patch.py
+++
@@ -1,5 +0,0 @@
-with open('app/teacher/layout.tsx', 'r') as f:
-    lines = f.readlines()
-print(f"Total lines: {len(lines)}")
-print("Line 268:", repr(lines[267]))
-print("Line 375:", repr(lines[374]))
