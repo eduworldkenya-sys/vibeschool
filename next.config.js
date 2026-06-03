689faa361b3f1bf30ecf4df4dad89faa0f9f76d2
Duncan <eduworldkenya@gmail.com>
2026-06-03 08:32:24 +0300

    fix: remove forced babel loader from webpack config

--- next.config.js
+++ next.config.js
@@ -3,18 +3,6 @@ const nextConfig = {
   compress: true,
   productionBrowserSourceMaps: false,
   swcMinify: false,
-  experimental: {
-    forceSwcTransforms: false,
-  },
-  webpack: (config, { defaultLoaders }) => {
-    config.resolve.fallback = { fs: false };
-    config.module.rules.push({
-      test: /\.(js|jsx|ts|tsx)$/,
-      exclude: /node_modules/,
-      use: [defaultLoaders.babel],
-    });
-    return config;
-  },
 };
 
 module.exports = nextConfig;
