7e18354aefbc668b7125f0ca04d28d1b7e926180
eduworldkenya-sys <eduworldkenya@gmail.com>
2026-08-09 23:38:53 +0300

    feat(hq): gate student Twin routes

---
+++ app/student/twin/layout.tsx
@@ -0,0 +1,5 @@
+import ProductRuntimeGate from "@/components/global/ProductRuntimeGate";
+
+export default function StudentTwinLayout({ children }: { children: React.ReactNode }) {
+  return <ProductRuntimeGate product="twin">{children}</ProductRuntimeGate>;
+}
