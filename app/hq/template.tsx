506ab7e73f7b061319de95ff85fce789234616f8
eduworldkenya-sys <eduworldkenya@gmail.com>
2026-08-09 23:59:35 +0300

    feat(hq): surface product nervous system status

---
+++ app/hq/template.tsx
@@ -0,0 +1,2 @@
+import HQProductNervousPulse from "@/components/hq/ProductNervousPulse";
+export default function HQTemplate({ children }: { children: React.ReactNode }) { return <><HQProductNervousPulse />{children}</>; }
