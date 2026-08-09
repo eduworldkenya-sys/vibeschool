2bcea701657c1c27bca1306996aa3b4ccdbb48b5
eduworldkenya-sys <eduworldkenya@gmail.com>
2026-08-09 23:59:20 +0300

    feat(hq): gate teacher twin runtime

---
+++ app/teacher/twin/layout.tsx
@@ -0,0 +1,2 @@
+import ProductRuntimeGate from "@/components/global/ProductRuntimeGate";
+export default function TeacherTwinLayout({ children }: { children: React.ReactNode }) { return <ProductRuntimeGate product="twin">{children}</ProductRuntimeGate>; }
