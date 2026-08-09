2f65d9b5d4a6ed4b35df715b2bb6208f0a0cf94d
eduworldkenya-sys <eduworldkenya@gmail.com>
2026-08-09 23:59:13 +0300

    feat(hq): gate student vibelearn runtime

---
+++ app/student/vibelearn/layout.tsx
@@ -0,0 +1,2 @@
+import ProductRuntimeGate from "@/components/global/ProductRuntimeGate";
+export default function StudentVibeLearnLayout({ children }: { children: React.ReactNode }) { return <ProductRuntimeGate product="vibelearn">{children}</ProductRuntimeGate>; }
